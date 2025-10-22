558f34809c4bd40a0cdca743cfbfc32b1627ff0c
Anderson Lourenco <andersonldev@gmail.com>
2025-10-22 20:42:31 -0300

    implementa detalhes da atividades e atualizações de navegação

--- app/src/main/java/com/example/gerenciadordeatividades/MainActivity.kt
+++ app/src/main/java/com/example/gerenciadordeatividades/MainActivity.kt
@@ -7,11 +7,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.gerenciadordeatividades.repository.TaskRepositoryImpl
 import com.example.gerenciadordeatividades.ui.screens.HomeScreen
+import com.example.gerenciadordeatividades.ui.screens.TaskDetailsScreen
 import com.example.gerenciadordeatividades.ui.theme.GerenciadorDeAtividadesTheme
 import com.example.gerenciadordeatividades.ui.viewmodel.TaskViewModel
 import com.example.gerenciadordeatividades.ui.viewmodel.TaskViewModelFactory
@@ -27,23 +30,43 @@ class MainActivity : ComponentActivity() {
 
                     val navController = rememberNavController()
 
+                    val context = LocalContext.current.applicationContext
+                    val taskManager = TaskManager(context)
+                    val taskRepository = TaskRepositoryImpl(taskManager)
+                    val viewModelFactory = TaskViewModelFactory(repository = taskRepository)
+
                     NavHost(
                         navController = navController,
                         startDestination = "home"
                     ) {
                         composable("home") {
 
-                            val context = LocalContext.current.applicationContext
-
-                            val taskManager = TaskManager(context)
-                            val taskRepository = TaskRepositoryImpl(taskManager)
-                            val viewModelFactory = TaskViewModelFactory(repository = taskRepository)
-
                             val viewModel: TaskViewModel = viewModel(factory = viewModelFactory)
 
-                            HomeScreen(viewModel = viewModel)
+                            HomeScreen(
+                                viewModel = viewModel, navController = navController
+                            )
+                        }
 
+                        composable("details/{taskId}",
+                            arguments = listOf(navArgument("taskId") {
+                                type = NavType.StringType
+                            })
+                        ) { backStackEntry ->
 
+                            val taskId = backStackEntry.arguments?.getString("taskId")
+
+                            if (taskId != null) {
+                                val viewModel: TaskViewModel = viewModel(factory = viewModelFactory)
+
+                                TaskDetailsScreen(
+                                    viewModel = viewModel,
+                                    taskId = taskId,
+                                    navController = navController
+                                )
+                            } else {
+                                navController.navigateUp()
+                            }
                         }
                     }
                 }
---
+++ app/src/main/java/com/example/gerenciadordeatividades/ui/screens/TaskDetailsScreen.kt
@@ -0,0 +1,125 @@
+package com.example.gerenciadordeatividades.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.gerenciadordeatividades.domain.model.TaskStatus
+import com.example.gerenciadordeatividades.ui.viewmodel.TaskViewModel
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TaskDetailsScreen(
+    viewModel: TaskViewModel,
+    taskId: String,
+    navController: NavController
+) {
+
+    val tasks by viewModel.tasks.collectAsState()
+
+    val task = remember(tasks, taskId) {
+
+        val foundTask = tasks.find { it.id == taskId }
+        foundTask
+    }
+    val utcFormatter = remember {
+        SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).apply {
+            timeZone = TimeZone.getTimeZone("UTC")
+        }
+    }
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        task?.title ?: "Carregando Atividade...",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigateUp() }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, "Voltar")
+                    }
+                },
+            )
+        }
+    ) { innerPadding ->
+
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            if (task == null) {
+                CircularProgressIndicator()
+            } else {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "Status: ${getStatusText(task.status)}",
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                        Text(
+                            text = "Limite: ${utcFormatter.format(Date(task.deadline))}",
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = MaterialTheme.colorScheme.secondary
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(24.dp))
+
+                    if (task.description.isNullOrBlank()) {
+                        Text(
+                        text = "Sem descrição",
+                        fontStyle = FontStyle.Italic,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    } else {
+                        Text(
+                            text = task.description,
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+fun getStatusText(status: TaskStatus): String {
+    return when (status) {
+        TaskStatus.PENDING -> "Pendente"
+        TaskStatus.IN_PROGRESS -> "Em Andamento"
+        TaskStatus.COMPLETED -> "Concluído"
+    }
+}
+
+
+
+
+
+

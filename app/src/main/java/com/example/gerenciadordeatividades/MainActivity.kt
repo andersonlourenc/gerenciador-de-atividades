ec51a4a950c1344c1231c66bc99f45ffca0d5e62
Anderson Lourenco <andersonldev@gmail.com>
2025-10-22 10:28:00 -0300

    implementação da Addtask e atualização homeScreen/MainActivity

--- app/src/main/java/com/example/gerenciadordeatividades/MainActivity.kt
+++ app/src/main/java/com/example/gerenciadordeatividades/MainActivity.kt
@@ -5,14 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.gerenciadordeatividades.repository.TaskRepositoryImpl
 import com.example.gerenciadordeatividades.ui.screens.HomeScreen
 import com.example.gerenciadordeatividades.ui.theme.GerenciadorDeAtividadesTheme
+import com.example.gerenciadordeatividades.ui.viewmodel.TaskViewModel
+import com.example.gerenciadordeatividades.ui.viewmodel.TaskViewModelFactory
+import com.example.gerenciadordeatividades.data.datastore.TaskManager
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContent {
             GerenciadorDeAtividadesTheme {
@@ -25,7 +32,18 @@ class MainActivity : ComponentActivity() {
                         startDestination = "home"
                     ) {
                         composable("home") {
-                            HomeScreen()
+
+                            val context = LocalContext.current.applicationContext
+
+                            val taskManager = TaskManager(context)
+                            val taskRepository = TaskRepositoryImpl(taskManager)
+                            val viewModelFactory = TaskViewModelFactory(repository = taskRepository)
+
+                            val viewModel: TaskViewModel = viewModel(factory = viewModelFactory)
+
+                            HomeScreen(viewModel = viewModel)
+
+
                         }
                     }
                 }
